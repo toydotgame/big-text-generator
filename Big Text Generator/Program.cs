83727e629ca703543e5b30611d440716d4898cb7
Redo122 <redo122@outlook.com>
2021-03-24 15:07:03 +1000

    Add project files.

---
+++ Big Text Generator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Big_Text_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line1 = { "╔═════╗", "╔════╗ ", "╔═════╗", "╔═════╗", "╔═════╗", "╔═════╗", "╔═════╗", "╔═╗ ╔═╗", "╔═══╗", "   ╔═╗", "╔═╗ ╔═╗", "╔═╗    ", " ╔══╗    ╔══╗ ", "╔══╗  ╔═╗", "╔═════╗", "╔═════╗", "╔═════╗ ", "╔═════╗", "╔════╗", "╔═════╗", "╔═╗ ╔═╗", "╔═╗  ╔═╗", "╔═╗  ╔══╗  ╔═╗", "╔══╗╔══╗", "╔═╗   ╔═╗", "╔═════╗" };
+            string[] line2 = { "║ ╔═╗ ║", "║ ╔╗ ║ ", "║ ╔═══╝", "╚╗ ╔╗ ║", "║ ╔═══╝", "║ ╔═══╝", "║ ╔═╗ ║", "║ ║ ║ ║", "╚╗ ╔╝", "   ║ ║", "║ ║╔╝╔╝", "║ ║    ", "╔╝  ╚╗  ╔╝  ╚╗", "║  ╚╗ ║ ║", "║ ╔═╗ ║", "║ ╔═╗ ║", "║ ╔═╗ ║ ", "║ ╔═╗ ║", "║ ╔══╝", "╚═╗ ╔═╝", "║ ║ ║ ║", "║ ║  ║ ║", "║ ║ ╔╝  ╚╗ ║ ║", "╚╗ ╚╝ ╔╝", "║ ╚╗ ╔╝ ║", "╚══╗  ║" };
+            string[] line3 = { "║ ║ ║ ║", "║ ╚╝ ╚╗", "║ ║    ", " ║ ║║ ║", "║ ╚══╗ ", "║ ╚══╗ ", "║ ║ ╚═╝", "║ ╚═╝ ║", " ║ ║ ", "   ║ ║", "║ ╚╝╔╝ ", "║ ║    ", "║ ╔╗ ╚╗╔╝ ╔╗ ║", "║ ╔╗╚╗║ ║", "║ ║ ║ ║", "║ ╚═╝ ║", "║ ║ ║ ║ ", "║ ╚═╝ ║", "║ ╚══╗", "  ║ ║  ", "║ ║ ║ ║", "║ ╚╗╔╝ ║", "║ ║╔╝ ╔╗ ╚╗║ ║", " ╚╗  ╔╝ ", "╚╗ ╚═╝ ╔╝", "  ╔╝ ╔╝" };
+            string[] line4 = { "║ ╚═╝ ║", "║ ╔═╗ ║", "║ ║    ", " ║ ║║ ║", "║ ╔══╝ ", "║ ╔══╝ ", "║ ║╔══╗", "║ ╔═╗ ║", " ║ ║ ", "╔═╗║ ║", "║ ╔╗╚╗ ", "║ ║    ", "║ ║╚╗ ╚╝ ╔╝║ ║", "║ ║╚╗╚╝ ║", "║ ║ ║ ║", "║ ╔═══╝", "║ ║ ║ ║ ", "║ ╔╗ ╔╝", "╚══╗ ║", "  ║ ║  ", "║ ║ ║ ║", "╚╗ ╚╝ ╔╝", "║ ╚╝ ╔╝╚╗ ╚╝ ║", " ╔╝  ╚╗ ", " ╚═╗ ╔═╝ ", " ╔╝ ╔╝ " };
+            string[] line5 = { "║ ╔═╗ ║", "║ ╚═╝ ║", "║ ╚═══╗", "╔╝ ╚╝ ║", "║ ╚═══╗", "║ ║    ", "║ ╚╝  ║", "║ ║ ║ ║", "╔╝ ╚╗", "║ ╚╝ ║", "║ ║╚╗╚╗", "║ ╚═══╗", "║ ║ ╚╗  ╔╝ ║ ║", "║ ║ ╚╗  ║", "║ ╚═╝ ║", "║ ║    ", "║ ╚═╝ ╚╗", "║ ║║ ╚╗", "╔══╝ ║", "  ║ ║  ", "║ ╚═╝ ║", " ╚╗  ╔╝ ", "╚╗  ╔╝  ╚╗  ╔╝", "╔╝ ╔╗ ╚╗", "   ║ ║   ", "╔╝  ╚═╗" };
+            string[] line6 = { "╚═╝ ╚═╝", "╚═════╝", "╚═════╝", "╚═════╝", "╚═════╝", "╚═╝    ", "╚═════╝", "╚═╝ ╚═╝", "╚═══╝", "╚════╝", "╚═╝ ╚═╝", "╚═════╝", "╚═╝  ╚══╝  ╚═╝", "╚═╝  ╚══╝", "╚═════╝", "╚═╝    ", "╚══════╝", "╚═╝╚══╝", "╚════╝", "  ╚═╝  ", "╚═════╝", "  ╚══╝  ", " ╚══╝    ╚══╝ ", "╚══╝╚══╝", "   ╚═╝   ", "╚═════╝" };
+            string word;
+            string path = @"c:\temp\BigText_Output.txt";
+            Console.WriteLine("╔════╗ ╔═══╗╔═════╗   ╔═══════╗╔═════╗╔══╗╔══╗╔═══════╗\n║ ╔╗ ║ ╚╗ ╔╝║ ╔═╗ ║   ║ ╔╗ ╔╗ ║║ ╔═══╝╚╗ ╚╝ ╔╝║ ╔╗ ╔╗ ║\n║ ╚╝ ╚╗ ║ ║ ║ ║ ╚═╝   ╚═╝║ ║╚═╝║ ╚══╗  ╚╗  ╔╝ ╚═╝║ ║╚═╝\n║ ╔═╗ ║ ║ ║ ║ ║╔══╗      ║ ║   ║ ╔══╝  ╔╝  ╚╗    ║ ║   \n║ ╚═╝ ║╔╝ ╚╗║ ╚╝  ║      ║ ║   ║ ╚═══╗╔╝ ╔╗ ╚╗   ║ ║   \n╚═════╝╚═══╝╚═════╝      ╚═╝   ╚═════╝╚══╝╚══╝   ╚═╝   \nEnter text below:");
+            word = Console.ReadLine();
+            Console.WriteLine("Text length = " + Convert.ToString(word.Length));
+            File.WriteAllText(path, String.Empty);
+            for (int i = 0; i < word.Length; i++)
+            {
+                int number = char.ToUpper(word[i]) - 65;
+                File.AppendAllText(path, line1[number]);
+            }
+            File.AppendAllText(path, "\n");
+            for (int i = 0; i < word.Length; i++)
+            {
+                int number = char.ToUpper(word[i]) - 65;
+                File.AppendAllText(path, line2[number]);
+            }
+            File.AppendAllText(path, "\n");
+            for (int i = 0; i < word.Length; i++)
+            {
+                int number = char.ToUpper(word[i]) - 65;
+                File.AppendAllText(path, line3[number]);
+            }
+            File.AppendAllText(path, "\n");
+            for (int i = 0; i < word.Length; i++)
+            {
+                int number = char.ToUpper(word[i]) - 65;
+                File.AppendAllText(path, line4[number]);
+            }
+            File.AppendAllText(path, "\n");
+            for (int i = 0; i < word.Length; i++)
+            {
+                int number = char.ToUpper(word[i]) - 65;
+                File.AppendAllText(path, line5[number]);
+            }
+            File.AppendAllText(path, "\n");
+            for (int i = 0; i < word.Length; i++)
+            {
+                int number = char.ToUpper(word[i]) - 65;
+                File.AppendAllText(path, line6[number]);
+            }
+            File.AppendAllText(path, "\n");
+            string readText = File.ReadAllText(path);
+            Console.WriteLine(readText + "\nText file created at " + path + "\nPress any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
