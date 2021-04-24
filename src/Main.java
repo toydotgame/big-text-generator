fa8b2210aa80ac4c975efaa06c2d55124458bdb7
toydotgame <toydotgame@gmail.com>
2021-04-24 14:51:04 +1000

    Base application - Charsets - Valid char checker - Crappy variables (TODO) - Unoptimised

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,15 @@ import java.util.Set;
 // TODO - `System.err` for errors, instead of `System.out`.
 
 public class Main {
-	static Set<String> charSet = DataStorage.charSet;	
+	static Set<String> charSet = DataStorage.charSet;
+	static String[] allowedChars = DataStorage.allowedChars;
+	static String[] bigText1 = DataStorage.bigText1;
+	static String[] bigText2 = DataStorage.bigText2;
+	static String[] bigText3 = DataStorage.bigText3;
+	static String[] bigText4 = DataStorage.bigText4;
+	static String[] bigText5 = DataStorage.bigText5;
+	static String[] bigText6 = DataStorage.bigText6;
+	static String allowedCharsString = DataStorage.allowedCharsString;
 	
 	public static void main(String[] args) {
 		System.out.println("╔════╗ ╔═══╗╔═════╗   ╔═══════╗╔═════╗╔══╗╔══╗╔═══════╗\n"
@@ -18,7 +26,7 @@ public class Main {
 		System.out.print("> ");
 		Scanner scanner = new Scanner(System.in); // Initialises user input, after the prompt.
 		
-		String input = scanner.nextLine();
+		String input = scanner.nextLine().toUpperCase();
 		for(int i = 0; i < input.length(); i++) {
 			String currentChar = String.valueOf(input.charAt(i)).toUpperCase();
 			boolean safe = charSet.contains(currentChar);
@@ -27,7 +35,47 @@ public class Main {
 				System.exit(128); // 128 is the Unix Exit Code for a bad argument. TODO: Untested
 			}
 		}
+		output(input);
 		System.out.println("Success.");
 		scanner.close();
+	}
+	
+	public static void output(String input) {
+		for(int i = 0; i < input.length(); i++) {
+			String currentChar = String.valueOf(input.charAt(i));
+			int charID = allowedCharsString.indexOf(currentChar);
+			System.out.print(bigText1[charID]);
+		}
+		System.out.print("\n");
+		for(int i = 0; i < input.length(); i++) {
+			String currentChar = String.valueOf(input.charAt(i));
+			int charID = allowedCharsString.indexOf(currentChar);
+			System.out.print(bigText2[charID]);
+		}
+		System.out.print("\n");
+		for(int i = 0; i < input.length(); i++) {
+			String currentChar = String.valueOf(input.charAt(i));
+			int charID = allowedCharsString.indexOf(currentChar);
+			System.out.print(bigText3[charID]);
+		}
+		System.out.print("\n");
+		for(int i = 0; i < input.length(); i++) {
+			String currentChar = String.valueOf(input.charAt(i));
+			int charID = allowedCharsString.indexOf(currentChar);
+			System.out.print(bigText4[charID]);
+		}
+		System.out.print("\n");
+		for(int i = 0; i < input.length(); i++) {
+			String currentChar = String.valueOf(input.charAt(i));
+			int charID = allowedCharsString.indexOf(currentChar);
+			System.out.print(bigText5[charID]);
+		}
+		System.out.print("\n");
+		for(int i = 0; i < input.length(); i++) {
+			String currentChar = String.valueOf(input.charAt(i));
+			int charID = allowedCharsString.indexOf(currentChar);
+			System.out.print(bigText6[charID]);
+		}
+		System.out.print("\n");
 	}
 }
